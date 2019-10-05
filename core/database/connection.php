a8779e0702d9b357f99e0f88c397baf9ac0f4e26
hojin-chu <cnghwls12@naver.com>
2019-10-06 01:12:57 +0900

    build in Login

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,12 +1,12 @@
 <?php
   // set Database variable information
   $dsn = "mysql:host=localhost; dbname=twitter";
-  $user = "root";
-  $password = "49600905";
+  $db_user = "root";
+  $db_password = "49600905";
 
   // Database PDO connection
   try {
-    $pdo = new PDO($dsn, $user, $password);
+    $pdo = new PDO($dsn, $db_user, $db_password);
   } catch(PDOExeception $e) {
     echo "Connection Error". $e -> getMessage();
   }
--- index.php
+++ index.php
@@ -2,3 +2,60 @@
   include 'core/init.php';
 
 ?>
+
+<!--
+   This template created by Meralesson.com 
+   This template only use for educational purpose 
+-->
+<html>
+	<head>
+		<title>twitter</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css"/>
+		<link rel="stylesheet" href="assets/css/style-complete.css"/>
+	</head>
+  <body>
+    <div class="front-img">
+      <img src="assets/images/home.jpg"></img>
+    </div>	
+
+    <div class="wrapper">
+      <div class="header-wrapper">
+        <div class="nav-container">
+          <div class="nav">
+            <div class="nav-left">
+              <ul>
+                <li><i class="fa fa-twitter" aria-hidden="true"></i><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+              </ul>
+            </div>
+            <div class="nav-right">
+              <ul>
+                <li><a href="#">Language</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="inner-wrapper">
+        <div class="main-container">
+          <div class="content-left">
+            <h1>Welcome to Tweety.</h1>
+            <br/>
+            <p>A place to connect with your friends — and Get updates from the people you love, And get the updates from the world and things that interest you.</p>
+          </div>
+          <div class="content-right">
+            <!-- Log In Section -->
+            <div class="login-wrapper">
+              <?php include "includes/login.php" ?>
+            </div>
+            <!-- Register Section -->
+            <div class="signup-wrapper">
+              <?php include "includes/register.php" ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
