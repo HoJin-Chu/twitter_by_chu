8b98b68c993e9f941e7d742ecdc71f9bd41b7d42
hojin-chu <cnghwls12@naver.com>
2019-10-18 11:23:32 +0900

    message recent user & search by user !!

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,4 +8,14 @@ $(() => {
       $('.search-result').html(data)
     })
   })
+
+  $(document).on('keyup', '.search-user', function() {
+    $('.message-recent').hide()
+    const search = $(this).val()
+    $.post('http://localhost/twitter/core/ajax/searchUserInMsg.php', {
+      search: search
+    }, (data) => {
+      $('.message-body').html(data)
+    })
+  })
 })
---
+++ core/ajax/searchUserInMsg.php
@@ -0,0 +1,28 @@
+<?php
+  include '../init.php';
+  if(isset($_POST['search']) && !empty($_POST['search'])) {
+    $user_id = $_SESSION['user_id'];
+    $search  = $getFromUser->checkInput($_POST['search']);
+    $result  = $getFromUser->search($search);
+
+    echo '<h4>People</h4><div class="message-recent">';
+
+    foreach ($result as $user) {
+      if($user->user_id != $user_id) {
+        echo '
+        <div class="people-message" data-user="'.$user->user_id.'">
+          <div class="people-inner">
+            <div class="people-img">
+              <img src="'.BASE_URL.$user->profileImage.'"/>
+            </div>
+            <div class="name-right">
+              <span><a>'.$user->screenName.'</a></span><span>@'.$user->username.'</span>
+            </div>
+          </div>
+        </div>
+        ';
+      }
+    }
+    echo '</div>';
+  }
+?>
